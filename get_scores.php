eb200018baf4a3a2cbda7dc4f44a8dbd0edfcc11
simoeile <simone7.brambilla@gmail.com>
2025-06-16 21:25:06 +0200

    Delete get_scores.php

--- get_scores.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$jsonFile = _DIR_ . '/scores.json';
-if (file_exists($jsonFile)) {
-    $content = file_get_contents($jsonFile);
-    echo $content;
-} else {
-    echo json_encode(new stdClass());
-}
